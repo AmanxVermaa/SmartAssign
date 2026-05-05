bef744c7ef60c3a45bb398859e0e77e6d5b0c599
Aman Verma <av9688754@gmail.com>
2026-05-05 22:39:09 +0530

    port-changes

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.get("/", (req, res) => {
     res.send("SmartAssign-AI Based Assignment Evaluation System Backend running");
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
