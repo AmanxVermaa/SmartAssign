58f1f133a16c86790906c8b1d2d88103cd06312a
Aman Verma <av9688754@gmail.com>
2026-05-06 20:46:46 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:"smart-assign-six.vercel.app",
+    credentials:true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
